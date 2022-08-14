0aed6071004b97f8b2cac8790cb0c3f51380a674
Nodir <istinniydowik228@gmail.com>
2022-08-14 18:03:14 +0500

    feat: Add chenge quiz by tap on button

--- code/script.js
+++ code/script.js
@@ -38,6 +38,7 @@ const a_textEl = document.getElementById("a_text")
 const b_textEl = document.getElementById("b_text")
 const c_textEl = document.getElementById("c_text")
 const d_textEl = document.getElementById("d_text")
+const submitBtnEl = document.getElementById("submitBtn")
 
 let currentQuiz = 0;
 
@@ -45,11 +46,23 @@ loadQuiz();
 
 function loadQuiz() {
 
-   const currentQuizData = quizData[currentQuiz]
+   const currentQuizData = quizData[currentQuiz];
    
    questionEl.innerText = currentQuizData.question
    a_textEl.innerText = currentQuizData.a
    b_textEl.innerText = currentQuizData.b
    c_textEl.innerText = currentQuizData.c
    d_textEl.innerText = currentQuizData.d
+
+   currentQuiz++;
 };
+
+
+submitBtnEl.addEventListener('click', () => {
+
+   if (currentQuiz < quizData.length) {
+      loadQuiz();
+   } else {
+      alert("You finished quiz")
+   }
+})
